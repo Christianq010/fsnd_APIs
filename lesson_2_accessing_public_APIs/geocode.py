3f45a97d08f35dbe6570c43395488019398da46d
Christiaan Quyn <christianq010@gmail.com>
2017-09-20 21:11:21 +0530

    refactor: Change whole response to only get lat/lng values from function

--- lesson_2_accessing_public_APIs/geocode.py
+++ lesson_2_accessing_public_APIs/geocode.py
@@ -4,7 +4,7 @@ import json
 def getGeocodeLocation(inputString):
     # Use Google Maps to convert a location into Latitute/Longitute coordinates
     # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-    google_api_key = "PASTE_YOUR_KEY_HERE"
+    google_api_key = "AIzaSyCV7N2v9gsZpEKH5-Wrf8vmno9NXMMLy9c"
     # replace all spaces with '+'
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'
@@ -13,16 +13,21 @@ def getGeocodeLocation(inputString):
     h = httplib2.Http()
     
     # Create a GET request that returns an array with 2 values, 
-    reponse, content = h.request(url,'GET')
+    response, content = h.request(url,'GET')
+    # result = json.loads(h.request(url,'GET')[1])
     # Call jason.loads on content, to format it better
     result = json.loads(content)
-    
+
     # Print to terminal
-    print "response header: %s \n \n" %reponse
+    # print "response header: %s \n \n" % response
 
-    return result
-    # latitude = result['results'][0]['geometry']['location']['lat']
-    # longitude = result['results'][0]['geometry']['location']['lng']
-    # return (latitude,longitude)
+    # return result
 
-getGeocodeLocation('Colombo Sri Lanka')
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude,longitude)
+
+# Store result in variable named dallas
+# dallas = "Dallas, Texas"
+
+# getGeocodeLocation(dallas)
