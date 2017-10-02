401253ac635d775962e291555c4d3edad8ec5e5d
Christiaan Quyn <christianq010@gmail.com>
2017-10-02 19:14:17 +0530

    refactor: Add hashing and verify password comments to models

--- securing_APIs/02_Adding Users and Logins/models.py
+++ securing_APIs/02_Adding Users and Logins/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+# package created for password hashing
 from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
@@ -11,9 +12,11 @@ class User(Base):
     username = Column(String(32), index=True)
     password_hash = Column(String(64))
 
+    # takes password as argument and stores hash in the user table
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
 
+    # this method will be called when a new user registers or changes their password on the system 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
